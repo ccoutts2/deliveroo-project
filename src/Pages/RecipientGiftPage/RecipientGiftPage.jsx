8ceb0b97cdc7d65fef81e008b728847cef7daf6f
Chris Coutts <chris.dcoutts@gmail.com>
2024-01-26 14:27:23 +0000

    Add gift modal

--- src/Pages/RecipientGiftPage/RecipientGiftPage.jsx
+++ src/Pages/RecipientGiftPage/RecipientGiftPage.jsx
@@ -1,9 +1,27 @@
 import "./RecipientGiftPage.scss";
+import giftScreen from "../../assets/images/gift-screen.svg";
+// import GiftModal from "../../components/GiftModal/GiftModal";
+import videoModal from "../../assets/images/video-modal.svg";
+import Popup from "reactjs-popup";
+import { useState } from "react";
 
 function RecipientGiftPage() {
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+  // const overlayStyle = { background: "rgba(0,0,0,0.5)" };
+  // const contentStyle = { background: "#000" };
+
   return (
     <main className="recipient-gift-page">
-      <div className="recipient-gift-page__container"></div>
+      <Popup open={open} closeOnDocumentClick onClose={closeModal} modal nested>
+        <div>
+          <img src={videoModal} />
+          <button className="button">Save Video</button>
+        </div>
+      </Popup>
+      <div className="recipient-gift-page__container">
+        <img src={giftScreen} onClick={() => setOpen((o) => !o)} />
+      </div>
     </main>
   );
 }
