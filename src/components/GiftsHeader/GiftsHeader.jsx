746c42960c95990a2feac806e6d6b437b3c7fe16
MalikP1 <m.pinkerton7@outlook.com>
2024-01-26 14:48:24 +0000

    link gifts page to home

--- src/components/GiftsHeader/GiftsHeader.jsx
+++ src/components/GiftsHeader/GiftsHeader.jsx
@@ -1,12 +1,12 @@
 import "./GiftsHeader.scss";
-
 import gift from "../../assets/images/gift.jpeg";
+import { Link } from "react-router-dom";
 
 function GiftsHeader() {
   return (
     <main className="gifts-header">
       <div className="gifts-header__container">
-        <img className="gifts-header__background-image" src={gift} />
+        <Link to="/"><img className="gifts-header__background-image" src={gift} /></Link>
       </div>
     </main>
   );
